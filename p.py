8b2a3d2cd15b5b10d5d1e6bd5c6de4b357d73aea
James Bouchat <80969731+heidelbergah@users.noreply.github.com>
2023-12-05 02:28:09 -0500

    Commented out visualize functions

--- p.py
+++ p.py
@@ -3,7 +3,7 @@ import penpal
 import canny_edge_detector as ced
 
 imgs = utility.load_data(dir_name='images')
-utility.visualize(imgs)
+# utility.visualize(imgs)
 
 detector = ced.cannyEdgeDetector(imgs, sigma=1.4, kernel_size=5, lowthreshold=0.09, highthreshold=0.17, weak_pixel=100)
 imgs_final = detector.detect()
@@ -11,7 +11,7 @@ edges = imgs_final[0]
 
 plot_points = []
 
-utility.visualize(imgs_final,'gray')
+# utility.visualize(imgs_final,'gray')
 
 for y in range(len(edges)-1):
     for x in range(len(edges[0])-1):
