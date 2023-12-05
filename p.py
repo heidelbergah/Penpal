bffa31115bb0e2e1fc428f6535ce00a59239d99a
James Bouchat <80969731+heidelbergah@users.noreply.github.com>
2023-12-04 20:47:53 -0500

    Edge Detection files

---
+++ p.py
@@ -0,0 +1,25 @@
+import utility
+import canny_edge_detector as ced
+
+imgs = utility.load_data(dir_name='images')
+utility.visualize(imgs, 'gray')
+
+detector = ced.cannyEdgeDetector(imgs, sigma=1.4, kernel_size=5, lowthreshold=0.09, highthreshold=0.17, weak_pixel=100)
+
+imgs_final = detector.detect()
+
+edges = imgs_final[0]
+
+type(edges)
+
+plot_points = []
+
+utility.visualize(imgs_final,'gray')
+
+for y in range(edges.size):
+    for x in range(edges[0].size):
+        if edges[x][y] > 0:
+            plot_points.append((x,y))
+            
+
+print(plot_points)
