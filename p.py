8066b627b57f8ebcc7ebaeb03e86043974ab1d1d
James Bouchat <80969731+heidelbergah@users.noreply.github.com>
2023-12-08 16:32:58 -0500

    Delete p.py

--- p.py
+++
@@ -1,9 +0,0 @@
-import utility
-import linedraw
-
-
-lines = linedraw.sketch("images/amongus.png")
-print(lines)
-utility.copyToPositionsTxt(lines)
-linedraw.visualize(lines)
-# penpal.drawTxtFile()
