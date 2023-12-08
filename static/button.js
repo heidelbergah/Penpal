1b4b85c4f9eec3dac5917d0b7eddfdc50dfbc7fa
James Bouchat <80969731+heidelbergah@users.noreply.github.com>
2023-12-08 16:37:01 -0500

    Uploaded button.js

--- static/button.js
+++ static/button.js
@@ -1,1 +1,21 @@
+var drawButton = $("#draw")
+drawButton.click(function(){
+    $.ajax({
+        url: "/draw_image",
+        type: "post",
+        success: function(response){
+            console.log("drawing image")
+        }
+    });
+});
 
+var danceButton = $("#dance")
+danceButton.click(function(){
+    $.ajax({
+        url: "/dance",
+        type: "post",
+        success: function(response){
+            console.log("Dancing!")
+        }
+    });
+});
